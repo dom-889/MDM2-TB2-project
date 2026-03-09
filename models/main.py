56498ff64e4fc400277254afdd234025c758ffaf
dom-889 <dominicon@icloud.com>
2026-03-09 15:40:23 +0000

    Update main.py

--- models/main.py
+++ models/main.py
@@ -1,4 +1,5 @@
-from model1 import grid_setup, video_capture
+from model2 import ring_thing, fan_setup, video_capture
+from functions_file import optimise_function_inputs
 import numpy as np
 from pick import pick
 import os
@@ -10,16 +11,18 @@ def main():
 
     while True:
 
-        if ipt == "y":
+        '''if ipt == "y":
             video_capture()
             grid_setup(fan_angle=np.pi/4, no_beams=50, ring_subdivisions=100, beam_subdivisions=100, image_string="test_image.png",show_plot=True)
-            break
+            break'''
 
-        elif ipt == "n":
+        if ipt == "n":
             options = [i for i in os.listdir(f"{os.getcwd()}/test_images")]
             selected = pick(options, title="Choose an image to reconstruct")
             print(f'You selected: {selected[0]}, reconstructing now...')
-            grid_setup(fan_angle=np.pi/4, no_beams=100, ring_subdivisions=200, beam_subdivisions=10, image_string=selected[0],show_plot=True)
+            fan_sd, ring_sd = optimise_function_inputs(selected[0])
+            fan_ls = fan_setup(np.pi/4, fan_sd)
+            ring_thing(fan_list=fan_ls, ring_subdivisions=ring_sd, beam_subdivisions=100, aperture=1, image_string=selected[0])
             break
 
         else:
