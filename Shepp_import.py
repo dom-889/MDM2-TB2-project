9666dbd42f121c85575b3081b2e100d5fa5dbd7f
myles-anderson <myles.anderson1404@outlook.com>
2026-04-04 20:54:22 +0100

    Shepp-logan phantom imported into test_images

--- Shepp_import.py
+++ Shepp_import.py
@@ -3,4 +3,28 @@ import matplotlib.pyplot as plt
 
 phantom = shepp_logan_phantom()
 
+plt.figure()
 plt.imshow(phantom, cmap='gray')
+plt.axis('off') 
+plt.savefig("test_images/shepp_logan_phantom.png", bbox_inches='tight', pad_inches=0)
+plt.close()  # Close figure to avoid overlap
+
+plt.figure(figsize=(2, 2), dpi=32)  # 2*32 = 64 pixels
+plt.imshow(phantom, cmap='gray')
+plt.axis('off') 
+plt.savefig("test_images/shepp_logan_phantom64x64.png", bbox_inches='tight', pad_inches=0)
+plt.close()
+
+
+'''
+plt.imshow(phantom, cmap='gray')
+plt.axis('off') 
+plt.savefig("test_images/shepp_logan_phantom.png", bbox_inches='tight', pad_inches=0)
+
+plt.figure(figsize=(2, 2), dpi=32)
+plt.imshow(phantom, cmap='gray')
+plt.axis('off') 
+plt.savefig("test_images/shepp_logan_phantom64x64.png", bbox_inches='tight', pad_inches=0)
+
+plt.show()
+'''
