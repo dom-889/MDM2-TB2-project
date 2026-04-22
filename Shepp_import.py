bf682bf3e9eca2cc261fb10dbb3f737c1f7b9e3a
myles-anderson <myles.anderson1404@outlook.com>
2026-04-22 13:38:16 +0100

    getting rid of more rubbish

--- Shepp_import.py
+++ Shepp_import.py
@@ -7,24 +7,5 @@ plt.figure()
 plt.imshow(phantom, cmap='gray')
 plt.axis('off') 
 plt.savefig("test_images/shepp_logan_phantom.png", bbox_inches='tight', pad_inches=0)
-plt.close()  # Close figure to avoid overlap
+plt.close() 
 
-plt.figure(figsize=(2, 2), dpi=32)  # 2*32 = 64 pixels
-plt.imshow(phantom, cmap='gray')
-plt.axis('off') 
-plt.savefig("test_images/shepp_logan_phantom64x64.png", bbox_inches='tight', pad_inches=0)
-plt.close()
-
-
-'''
-plt.imshow(phantom, cmap='gray')
-plt.axis('off') 
-plt.savefig("test_images/shepp_logan_phantom.png", bbox_inches='tight', pad_inches=0)
-
-plt.figure(figsize=(2, 2), dpi=32)
-plt.imshow(phantom, cmap='gray')
-plt.axis('off') 
-plt.savefig("test_images/shepp_logan_phantom64x64.png", bbox_inches='tight', pad_inches=0)
-
-plt.show()
-'''
--- test_folder/Myles_test.py
+++
@@ -1,2 +0,0 @@
-for i in range(3):
-    print('Is this working', i+1)
